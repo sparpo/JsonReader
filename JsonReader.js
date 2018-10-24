875107c40b0aa23d502f7966dc13b27b215e0302
Joey <joeyecorbett@Gmail.com>
2018-10-24 10:50:19 +0100

    Create JsonReader.js

---
+++ JsonReader.js
@@ -0,0 +1,10 @@
+var compareData = function(output) {
+  var answer = false;
+  if(output = "test") {
+  answer = true;
+  }
+  return answer;
+}
+loadJson = function(jsonFile) {
+var mydata = JSON.parse(jsonFile);
+}
