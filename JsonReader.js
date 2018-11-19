d288bffa3fd572dabaf0eb0424bd749c946f29d5
Joey <joeyecorbett@Gmail.com>
2018-11-19 13:44:43 +0000

    Update JsonReader.js

--- JsonReader.js
+++ JsonReader.js
@@ -1,3 +1,13 @@
+document.getElementById("answer").addEventListener(search);
+var targetAns = document.getElementById("answer").value;
+var actualAns = 
+var n = str.search("answer");
+document.getElementById("demo").innerHTML = n;
+if(n > -1) {
+  document.getElementById("demo").innerHTML = "Correct";
+} else {
+  document.getElementById("demo").innerHTML = "False";
+}
 var compareData = function(output) {
   var answer = false;
   if(output = "test") {
